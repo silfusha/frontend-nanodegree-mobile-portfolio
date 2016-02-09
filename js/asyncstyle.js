2789f53b30eaa4b2ea71979f0a767fa4b411d63f
Lisitski Sergey <silfusha@gmail.com>
2016-02-09 04:52:31 +0300

    Signed-off-by: Lisitski Sergey <silfusha@gmail.com>

---
+++ js/asyncstyle.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Sergey on 09.02.2016.
+ */
+function goAsyncStyle() {
+var resource = document.createElement('link');
+resource.setAttribute("rel", "stylesheet");
+resource.setAttribute("href","css/style.css");
+resource.setAttribute("type","text/css");
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(resource);
+}
+window.addEventListener("load", function(event) {
+    goAsyncStyle();
+});
